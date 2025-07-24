6adae2febf93113a8789491c5e4465e1f5c7bcbf
Naswade21 <naswade21@gmail.com>
2025-07-24 18:11:33 -0400

    Got Stuck Part 1

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//When someone clicks each button except the game-sealing 3 pointer, it should add 1-2-3
+//When every player starts, they have no buckets
+let playerOne = 0
+let playerTwo = 0
+let playerThree = 0
+
+let pOne = document.getElementById("p-one")
+let pTwo = document.getElementById("p-two")
+let pThree = document.getElementById("p-three")
+
+//Pseudo - When a player scores(1-2-3) then there counter goes up. If I'm player one, and I score a basket thats a free throw I should get one points
+//I score a two pointer then it should add on to the *current* score whatever it is, same with a three pointer
+// When a player reaches 21, the game-sealing activates. Otherwise it does nothing
+// After game-sealing 3 activates and is clicked that player(1-2-3) will win
+// When a player wins, they get added to the winner list and previous winner list until knocked out
+// When a new player wins, they get added to the winner and the previous winner gets displayed
+
+function scoreFreethrow(){
+    
+}
+
+function scoreTwop(){
+    
+}
+
+function scoreThreep(){
+    
+}
+
+function pOnecounter(){
+
+}
+
+
+
+
